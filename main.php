1b80b3099301fad03e52ed1e8eba0bba55b7a5a8
Kenny Shi <kenshi@ucla.edu>
2014-04-05 00:33:35 -0700

    testing stuff

--- main.php
+++ main.php
@@ -3,8 +3,8 @@
 <head>
     <script src="/js/vendor/modernizr.js"></script>
     <script src="/js/vendor/jquery.js"></script>
-    <script type='text/javascript' src="js/typeahead.bundle.js"></script>
-    <script type='text/javascript' src="js/search.js"></script>
+    <script type='text/javascript' src="/js/typeahead.bundle.js"></script>
+    <script type='text/javascript' src="/js/search.js"></script>
 </head>
 <body>
     <div id="search">
