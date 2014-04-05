9bf7cbf5b632ff1137871bd51bcacadab5012252
Soomin Jeong <sjeongus@gmail.com>
2014-04-04 22:32:54 -0700

    Added addClass function

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-function fbLogin() {
-	var fburl = 'https://la-chats.firebaseio.com/';
+var myUser;
+var fburl = 'https://la-chats.firebaseio.com/';
 
-	var chatRef = new Firebase(fburl);
+function fbLogin() {
+
+	var fbRef = new Firebase(fburl);
 	var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
 	  if (error) {
 	    // an error occurred while attempting login
-	    switch(error.code) {
-	      case 'INVALID_EMAIL':
-	      case 'INVALID_PASSWORD':
-	      default:
+	    console.log(error);
 	    }
 	  } else if (user) {
 	    // user authenticated with Firebase
 	    console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
-	    var userRef = chatRef.child('users/' + user.id);
+	    myUser = user;
+	    var userRef = chatRef.child('users').child(user.id);
 
 	    userRef.child('name').set(user.displayName);
+	    userRef.child('fbtoken').set(user.accessToken);
 
 	  } else {
 	    // user is logged out
@@ -23,4 +24,17 @@ function fbLogin() {
 	});
 
 	auth.login('facebook');
+}
+
+function addClass(classes) {
+
+	var fbRef = new Firebase(fburl);
+	var classLen = classes.length;
+	for (var i = 0; i < classLen; i++) {
+		// classes[i] should be a dict containting the class data
+		var userRef = fbRef.child('users').child(user.id).child(classes).child(classes[i]);
+		userRef.child('classid').set(classes[i]);
+		var classRef = chatRef.child(classes[i]).child(myUser.id);
+		classRef.child('name').set(myUser.displayName);
+	}
 }
