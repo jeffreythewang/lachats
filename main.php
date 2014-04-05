38573a408346ad9e5b8004bcba6edb68555e7f76
Kenny Shi <kenshi@ucla.edu>
2014-04-05 00:52:24 -0700

    switching to business again

--- main.php
+++ main.php
@@ -4,11 +4,10 @@
     <script src="/js/vendor/modernizr.js"></script>
     <script src="/js/vendor/jquery.js"></script>
     <script type='text/javascript' src="/js/typeahead.bundle.js"></script>
-    <script type='text/javascript' src="/js/search.js"></script>
 </head>
 <body>
-    <div id="searching">
-      <input type="text" id="search" data-provide="typeahead" data-items="4" placeholder="Classes at UCLA">
+    <div id="search">
+      <input class="typeahead" type="text" placeholder="Classes at UCLA">
     </div>
 </body>
 
